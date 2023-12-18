553425615954674b91ce057bb85b0b8d539c847f
miyabiit <yamada.masahiro@gmail.com>
2023-12-18 17:48:57 +0900

    add password (global type) code

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import streamlit as st
 import datetime as dt
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
+import hmac
 
 # Warningの非表示
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
+# データの取得と表示
 def get_data(start, end, id):
     df = web.DataReader(id, 'fred', start, end)
     return df
@@ -26,6 +28,36 @@ def plot_graph(start):
     ax_usa.legend()
     
     st.pyplot()
+
+# グローバルパスワード用のコード
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+# ここまでがパスワードチェック用
 
 min_date = dt.date(2012,1,1)
 max_date = dt.date.today()
